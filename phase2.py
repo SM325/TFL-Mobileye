4419ae0f9694276e753ae4d3e23afe4b2b9f52ef
Sara Mordehayov <saramor325@gmail.com>
2020-12-21 18:09:39 +0200

    crop picture1

--- phase2.py
+++ phase2.py
@@ -25,10 +25,28 @@ def is_contain_tfl_by_img(labled_img):
 
 
 def is_contain_tfl_by_img_and_cord(labled_img, cord):
-    path_ = './data/gtFine/train/aachen/aachen_000010_000019_gtFine_labelIds.png'
-    labled_img = np.array(Image.open(path_))
+    # path_ = './data/gtFine/train/aachen/aachen_000010_000019_gtFine_labelIds.png'
+    # labled_img = np.array(Image.open(path_))
     return labled_img[cord[0], cord[1]] == 19
 
+
+def crop_img_by_center(img, cord):
+    path_ = './data/leftImg8bit/train/aachen/aachen_000010_000019_leftImg8bit.png'
+    img = np.array(Image.open(path_))
+    height, width, _ = img.shape
+    left = cord[0] - 40
+    top = cord[1] - 40
+    right = cord[0] + 41
+    bottom = cord[1] + 41
+    # if left < 0:
+    #     padding_size = -left
+    #     padding_arr = np.zeros((padding_size * height * 3), dtype=img.dtype).reshape(( height, padding_size, 3))
+    #     img = np.hstack((padding_arr, img))
+    #     left += padding_size
+    #     right += padding_size
+    # plt.imshow(img)
+    return Image.fromarray(img).crop((left, top, right, bottom))
+    # plt.imshow(res)
 
 def main(argv=None):
     original_images_base = './data/leftImg8bit'
@@ -50,6 +68,9 @@ def main(argv=None):
     #     plt.show(block=True)
 
 
+
 if __name__ == '__main__':
     # main()
-    is_contain_tfl_by_img("s")
+    # is_contain_tfl_by_img("s")
+    plt.imshow(crop_img_by_center(0, (2048, 200)))
+    print("end")
